441113f23b8232c80d349799ab53823744304d25
David Arnold <david.arnold@online-rewards.com>
2026-06-19 11:40:08 -0400

    Complete chapter 2

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -7,22 +7,14 @@ defmodule Tunez.Music.Artist do
   end
 
   actions do
-    # create :create do
-    #   accept [:name, :biography]
-    # end
+    update :update do
+      require_atomic? false
+      accept [:name, :biography]
 
-    # read :read do
-    #   primary? true
-    # end
+      change Tunez.Music.Changes.UpdatePreviousNames, where: [changing(:name)]
+    end
 
-    # update :update do
-    #   accept [:name, :biography]
-    # end
-
-    # destroy :destroy do
-    # end
-
-    defaults [:create, :read, :update, :destroy]
+    defaults [:create, :read, :destroy]
     default_accept [:name, :biography]
   end
 
@@ -33,9 +25,19 @@ defmodule Tunez.Music.Artist do
       allow_nil? false
     end
 
+    attribute :previous_names, {:array, :string} do
+      default []
+    end
+
     attribute :biography, :string
 
     create_timestamp :inserted_at
     update_timestamp :updated_at
+  end
+
+  relationships do
+    has_many :albums, Tunez.Music.Album do
+      sort year_released: :desc
+    end
   end
 end
