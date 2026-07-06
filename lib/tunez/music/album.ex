ad082f210d107acce1ee76bc0f06597ded764731
David Arnold <david.arnold@online-rewards.com>
2026-07-06 16:26:21 -0400

    Complete chapter 4 (skip GraphQL)

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -1,5 +1,13 @@
 defmodule Tunez.Music.Album do
-  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+  use Ash.Resource,
+    otp_app: :tunez,
+    domain: Tunez.Music,
+    data_layer: AshPostgres.DataLayer,
+    extensions: [AshJsonApi.Resource]
+
+  json_api do
+    type "album"
+  end
 
   postgres do
     table "albums"
